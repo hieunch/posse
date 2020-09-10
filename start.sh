203fccf6cfb3a0acd0970027ca9d0fe03dbd1f2c
Hieu Nguyen <chihieu2506@gmail.com>
2020-09-10 15:21:58 +0700

    add logs folder

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
-cd Castalia/Castalia
-./makemake
-make
-cd ../..
+# cd Castalia/Castalia
+# ./makemake
+# make
+# cd ../..
 cd client
 yarn
 yarn start &
 cd ..
 cd Castalia/Castalia/Simulations/server
+mkdir logs
 yarn
 yarn run dev &
 
